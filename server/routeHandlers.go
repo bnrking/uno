c23e575cbe17b29a5a328aef539d1dc04f25d742
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-07-30 14:36:58 -0600

    Run gofmt.

--- server/routeHandlers.go
+++ server/routeHandlers.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"net/http"
-    "github.com/jak103/uno/db"
+
+	"github.com/jak103/uno/db"
 	"github.com/jak103/uno/model"
 	"github.com/labstack/echo/v4"
 )
@@ -14,63 +15,64 @@ type Response struct {
 	Payload   map[string]interface{} `json:"payload"`
 }
 
+type request = map[string]interface{}
+
 func setupRoutes(e *echo.Echo) {
-	e.GET("/newgame/", newGame)
+	e.POST("/games", newGame)
 	e.GET("/update", update)
 	e.POST("/startgame", startGame)
 	e.POST("/login/:username", login)
-    e.POST("/joinGame/:game", join)
+	e.POST("/joinGame/:game", join)
 	e.POST("/play/:number/:color", play)
 	e.POST("/draw", draw)
 }
 
 func newGame(c echo.Context) error {
 	game, gameErr := createNewGame()
-    
-    if  gameErr != nil {
+
+	if gameErr != nil {
 		return gameErr
 	}
-    
-    return c.JSONPretty(http.StatusOK, &Response{true, newPayload(game)}, "  ")
+	return c.JSONPretty(http.StatusOK, &Response{true, newPayload(game)}, "  ")
 }
 
 func login(c echo.Context) error {
 	username := c.Param("username")
-    
-    database, err := db.GetDb()
+
+	database, err := db.GetDb()
 	if err != nil {
 		return err
 	}
-    
-    player, playerErr := database.CreatePlayer(username)
+
+	player, playerErr := database.CreatePlayer(username)
 
 	if playerErr != nil {
 		return playerErr
 	}
-    
-    token, err := newJWT(username, player.ID);
-    
-    if err != nil {
+
+	token, err := newJWT(username, player.ID)
+
+	if err != nil {
 		return err
 	}
-    
+
 	return c.JSONPretty(http.StatusOK, &Response{true, makeJWTPayload(token)}, "  ")
 }
 
 func join(c echo.Context) error {
-    authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
-    player, validPlayer, err := getPlayerFromHeader(authHeader)
-    
-    if err != nil {
+	authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
+	player, validPlayer, err := getPlayerFromHeader(authHeader)
+
+	if err != nil {
 		return err
 	}
-    
-    if !validPlayer {
-        return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
-    }
-    
+
+	if !validPlayer {
+		return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
+	}
+
 	game, err := joinGame(c.Param("game"), player)
-    
+
 	if err != nil {
 		return err
 	}
@@ -79,33 +81,33 @@ func join(c echo.Context) error {
 }
 
 func startGame(c echo.Context) error {
-    authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
-    player, validPlayer, err := getPlayerFromHeader(authHeader)
-    
-    if err != nil {
+	authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
+	player, validPlayer, err := getPlayerFromHeader(authHeader)
+
+	if err != nil {
 		return err
 	}
-    
-    if !validPlayer {
-        return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
-    }
-    
+
+	if !validPlayer {
+		return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
+	}
+
 	dealCards(c.Param("game"), player)
 	return update(c)
 }
 
 func update(c echo.Context) error {
-    authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
-    player, validPlayer, err := getPlayerFromHeader(authHeader)
-    
-    if err != nil {
+	authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
+	player, validPlayer, err := getPlayerFromHeader(authHeader)
+
+	if err != nil {
 		return err
 	}
-    
-    if !validPlayer {
-        return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
-    }
-    
+
+	if !validPlayer {
+		return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
+	}
+
 	game, err := updateGame(c.Param("game"), player)
 	if err != nil {
 		return err
@@ -116,18 +118,18 @@ func update(c echo.Context) error {
 func play(c echo.Context) error {
 	// TODO Cards have a value, which can include skip, reverse, etc
 	card := model.Card{c.Param("number"), c.Param("color")}
-    
-    authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
-    player, validPlayer, err := getPlayerFromHeader(authHeader)
-    
-    if err != nil {
+
+	authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
+	player, validPlayer, err := getPlayerFromHeader(authHeader)
+
+	if err != nil {
 		return err
 	}
-    
-    if !validPlayer {
-        return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
-    }
-    
+
+	if !validPlayer {
+		return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
+	}
+
 	game, err := playCard(c.Param("game"), player, card)
 
 	if err != nil {
@@ -138,16 +140,16 @@ func play(c echo.Context) error {
 }
 
 func draw(c echo.Context) error {
-    authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
-    player, validPlayer, err := getPlayerFromHeader(authHeader)
-    
-    if err != nil {
+	authHeader := c.Request().Header.Get(echo.HeaderAuthorization)
+	player, validPlayer, err := getPlayerFromHeader(authHeader)
+
+	if err != nil {
 		return err
 	}
-    
-    if !validPlayer {
-        return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
-    }
+
+	if !validPlayer {
+		return c.JSONPretty(http.StatusUnauthorized, &Response{false, nil}, " ")
+	}
 
 	game, err := drawCard(c.Param("game"), player)
 
